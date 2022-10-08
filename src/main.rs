4cef5a0c61b186ddcb13c2cface8d578811175df
Patrick <patrickburns2557@gmail.com>
2022-10-07 19:41:18 -0500

    hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,25 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32
-}
-
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        (self.width > other.width) && (self.height > other.height)
-    }
-    
-}
-
+use std::collections::HashMap;
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50
-    };
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40
-    };
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45
-    };
+    let mut scores = HashMap::new();
 
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    scores.insert(String::from("Blue"), 10);
     
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}\n\n", scores);
+
+
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
