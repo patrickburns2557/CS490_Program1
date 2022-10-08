520b89fcc1b4523c3e17f6a9fb95172fb39d87d5
Patrick <patrickburns2557@gmail.com>
2022-10-07 19:41:30 -0500

    clear

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,3 @@
-use std::collections::HashMap;
-
 fn main() {
-    let mut scores = HashMap::new();
-
-    scores.insert(String::from("Blue"), 10);
     
-    scores.entry(String::from("Yellow")).or_insert(50);
-    scores.entry(String::from("Blue")).or_insert(50);
-
-    println!("{:?}\n\n", scores);
-
-
-
-    let text = "hello world wonderful world";
-
-    let mut map = HashMap::new();
-
-    for word in text.split_whitespace() {
-        let count = map.entry(word).or_insert(0);
-        *count += 1;
-    }
-
-    println!("{:?}", map);
 }
