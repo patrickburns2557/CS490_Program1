4f797fed7cd869b0e7bb7ac3500175a77d106712
Patrick <patrickburns2557@gmail.com>
2022-10-11 22:59:49 -0500

    Output messages and get user input

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ order of priority
 **********************************/
 
 /* Use declarations */
-use std::{collections::{VecDeque, BinaryHeap}, cmp::Ordering};
+use std::{collections::{VecDeque, BinaryHeap}, cmp::Ordering, io};
 use rand::Rng;
 
 /* Structs and their associated Methods */
@@ -40,10 +40,10 @@ impl Process {
     }
 }
 //Implements ordering to be used in the Binary Heap to sort the Process instances by priority
-//Reverses the normally expected output of cmp to create a min binary heap
+//Reverses the normally expected output of cmp to create a minimum binary heap
 impl Ord for Process {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        //Reverse the Ordering result to create a min binary heap
+        //Reverse the Ordering result to create a minimum binary heap
         match self.priority {
             b if b > other.priority => Ordering::Less,
             b if b < other.priority => Ordering::Greater,
@@ -52,13 +52,13 @@ impl Ord for Process {
         }
     }
 }
-//Implements PartialOrd for Process to be used by Ord
+//Implements PartialOrd for Process to use Ord
 impl PartialOrd for Process {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
     }
 }
-//Implements PartialEq f or Process to be used by Ord
+//Implements PartialEq for Process to use Ord
 impl PartialEq for Process {
     fn eq(&self, other: &Self) -> bool {
         self.priority == other.priority
@@ -66,32 +66,49 @@ impl PartialEq for Process {
 }
 
 fn main() {
+    let mut input = String::new();
     let mut num_processes: u32 = 0;
     let mut process_queue: VecDeque<Process> = VecDeque::new();
     let mut process_binary_heap: BinaryHeap<Process> = BinaryHeap::new();
     
+    println!("Enther the number of process nodes you wish to generate:");
+
+    //Read in input from user
+    io::stdin().read_line(&mut input).expect("Failed to read input.");
+    print!("\n");
+    //parse integer out of the input. If input is invalid, program lets user know and exits
+    let input: u32 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {println!("Invalid input."); return;}
+    };
     
-    //Generate process nodes and push them onto the queue GET USER INPUT FOR THIS LATER
-    for _i in 1..=10 {
+    println!("Creating and adding {} process nodes to the Queue and the Binary MinHeap.", input);
+    //Generate process nodes and push them onto the queue
+    for _i in 1..=input {
         num_processes += 1;
         let p1 = Process::new(num_processes);
         process_queue.push_back(p1);
     }
+    println!("Successfully added {} process nodes added to the Queue.", process_queue.len());
 
     //Copy the process nodes from the queue and place the copies into the binary heap
     for i in &process_queue {
         process_binary_heap.push(i.clone());
     }
+    println!("Successfully added {} process nodes added to the Binary MinHeap.", process_binary_heap.len());
 
-
-    for _i in 0..=(num_processes-1) {
-        let p = process_binary_heap.pop().unwrap();
-        println!("Id: {:>3}, priority: {:>4}, sleep time: {:>4}, description: {}", p.id, p.priority, p.sleep_time, p.description);
-    }
-    println!("\n\n");
+    print!("\n");
+    println!("Draining the Queue, one process at a time:");
     for _i in 0..=(num_processes-1) {
         let p = process_queue.pop_front().unwrap();
-        println!("Id: {:>3}, priority: {:>4}, sleep time: {:>4}, description: {}", p.id, p.priority, p.sleep_time, p.description);
+        println!(" | Id: {:>5} |  priority: {:>6} |  sleep time: {:>6} |  description: {}", p.id, p.priority, p.sleep_time, p.description);
+    }
+    print!("\n");
+
+    println!("Draining the Binary MinHeap, one process at a time:");
+    for _i in 0..=(num_processes-1) {
+        let p = process_binary_heap.pop().unwrap();
+        println!(" | Id: {:>5} |  priority: {:>6} |  sleep time: {:>6} |  description: {}", p.id, p.priority, p.sleep_time, p.description);
     }
     
 }
